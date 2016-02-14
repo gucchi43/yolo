9797b36eb65c66f843daee637e8578b21d4a8467
FumiYama <f.yamanaka1127@gmail.com>
2016-02-14 15:49:17 +0900

    Add TimeLine View Function

---
+++ masterch/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  masterch
+//
+//  Created by Fumiya Yamanaka on 2016/02/14.
+//  Copyright © 2016年 Fumiya Yamanaka. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var postTextLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
