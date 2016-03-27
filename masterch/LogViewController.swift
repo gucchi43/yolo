9091bf84cb8b95517c4ef796695b0e171fd047c3
Eisuke Sato <e.sato@livingstyle.co.jp>
2016-03-28 01:01:46 +0900

    LogViewControllerのtableView実装

--- masterch/LogViewController.swift
+++ masterch/LogViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 
-class LogViewController: UIViewController {
+class LogViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var toggleWeek: Bool = false
+    var items: NSArray = NSArray()
     
     @IBOutlet weak var calendarBaseView: UIView!
     @IBOutlet weak var calendarWeekView: UIView!
     
+    @IBOutlet weak var tableView: UITableView!
     
     var calendarView: CalendarView?
     var calendarAnotherView: CalendarAnotherView?
@@ -23,14 +25,21 @@ class LogViewController: UIViewController {
     @IBOutlet weak var menuView: UIView!
     @IBOutlet weak var monthLabel: UILabel!
     
+    var selectedPostImage: UIImage!
+    var selectedPostText: String!
+    var selectedPostDate: String!
+    
     var animationFinished = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("LogViewController")
-        
+        tableView.estimatedRowHeight = 370
+        tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.registerNib(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "postTableViewCell")
         monthLabel.text = "2016年2月"
+        loadItems()
     }
     
     override func didReceiveMemoryWarning() {
@@ -48,7 +57,88 @@ class LogViewController: UIViewController {
         }
     }
     
+    func loadItems() {
+        let query: NCMBQuery = NCMBQuery(className: "Post")
+        query.orderByDescending("postDate") // cellの並べ方
+        query.findObjectsInBackgroundWithBlock({(NSArray objects, NSError error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                if objects.count > 0 {
+                    self.items = objects
+                    self.tableView.reloadData()
+                }
+            }
+        })
+    }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellId = "postTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath) as! CustomTableViewCell
+        //        各値をセルに入れる
+        let postData = items[indexPath.row]
+        //        postTextLabelには(key: "text")の値を入れる
+        cell.postTextLabel.text = postData.objectForKey("text") as? String
+        cell.postDateLabel.text = postData.objectForKey("postDate") as? String
+        
+        //        画像データの取得
+        if (postData.objectForKey("image1") == nil) { // 複数投稿の時にはどうにかしたいコード(if文)
+            cell.postImageView.image = nil
+        } else {
+            let postImageName = (postData.objectForKey("image1") as? String)!
+            let postImageData = NCMBFile.fileWithName(postImageName, data: nil) as! NCMBFile
+            
+            postImageData.getDataInBackgroundWithBlock { (imageData: NSData?, error: NSError!) -> Void in
+                if error != nil {
+                    print("写真の取得失敗: \(error)")
+                } else {
+                    cell.postImageView.image = UIImage(data: imageData!)
+                }
+            }
+        }
+        
+        //        TableView にある特定の Cell のアクセサリーをつけない
+        cell.accessoryType = UITableViewCellAccessoryType.None
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("セルの選択: \(indexPath.row)")
+        let postData = self.items[indexPath.row]
+        
+        selectedPostText = postData.objectForKey("text") as? String
+        selectedPostDate = postData.objectForKey("postDate") as? String
+        
+        //        画像データの取得
+        if postData.objectForKey("image1") != nil { // 複数投稿の時にはどうにかしたいコード(if文)
+            let postImageName = (postData.objectForKey("image1") as? String)!
+            let postImageData = NCMBFile.fileWithName(postImageName, data: nil) as! NCMBFile
+            
+            postImageData.getDataInBackgroundWithBlock { (imageData: NSData?, error: NSError!) -> Void in
+                if error != nil {
+                    print("写真の取得失敗: \(error)")
+                } else {
+                    self.selectedPostImage = UIImage(data: imageData!)
+                }
+            }
+        }
+        
+        performSegueWithIdentifier("toPostDetailViewController", sender: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "toPostDetailViewController" {
+            let postDetailVC: PostDetailViewController = segue.destinationViewController as! PostDetailViewController
+            
+            postDetailVC.postDateText = selectedPostDate
+            postDetailVC.postText = selectedPostText
+            postDetailVC.postImage = selectedPostImage
+        }
+    }
     
     @IBOutlet weak var weekConstraint: NSLayoutConstraint!
     @IBOutlet weak var monthConstraint: NSLayoutConstraint!
