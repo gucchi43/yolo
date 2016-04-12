da27faf4795bc397659ff8f0f5946b49a91b6480
Hiroki Taniguchi <acmikn43@gmail.com>
2016-04-12 11:26:22 +0900

    add SetProfileViewController.swift & view #68

---
+++ masterch/SetProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SetProfileViewController.swift
+//  masterch
+//
+//  Created by HIroki Taniguti on 2016/04/12.
+//  Copyright © 2016年 Fumiya Yamanaka. All rights reserved.
+//
+
+import UIKit
+
+class SetProfileViewController: UIViewController {
+    
+
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
