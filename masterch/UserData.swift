90250ec724db731bf9aeaf31a152bfbb06992cfc
Hiroki Taniguchi <acmikn43@gmail.com>
2016-04-20 18:12:30 +0900

    UserDataをシングルトン型にupdate #70

--- masterch/UserData.swift
+++ masterch/UserData.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
- class UserData: UIViewController {
+
+//@@@多分シングルトン機能している
+//シングルトン実装
+ class UserData {
+    static var sharedUserData: UserData = {
+        return UserData()
+    }()
+    
+    private init(){
+    }
     let newUser = NCMBUser()
     
     let user = NCMBUser.currentUser()
@@ -16,5 +25,4 @@ import UIKit
     let userFaceName = NCMBUser.currentUser().objectForKey("userFaceName")
     let twitterName = NCMBUser.currentUser().objectForKey("twitterName")
     let facebookName = NCMBUser.currentUser().objectForKey("facebookName")
-    
 }
