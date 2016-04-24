21b0e1c7c2fadfd71d0097dc47ef247fee04bae5
FumiYama <f.yamanaka1127@gmail.com>
2016-04-24 21:39:55 +0900

    フォローボタンでフォロー&解除の機能を追加

--- masterch/PostDetailViewController.swift
+++ masterch/PostDetailViewController.swift
@@ -23,13 +23,14 @@ class PostDetailViewController: UIViewController {
     @IBOutlet weak var followButton: UIButton!
     
     var postObject: NCMBObject!
+    var isFollowing: Bool = false
+    var followingRelationshipObject: NCMBObject = NCMBObject()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let author = postObject.objectForKey("user") as? NCMBUser
-        if let author = author {
+        if let author = postObject.objectForKey("user") as? NCMBUser {
             userProfileNameLabel.text = author.objectForKey("userFaceName") as? String
             userNameLabel.text = author.userName
             let userImageData = NCMBFile.fileWithName(author.objectForKey("userProfileImage") as? String, data: nil) as! NCMBFile
@@ -70,19 +71,80 @@ class PostDetailViewController: UIViewController {
         } else {
             postImageViewHeightConstraint.constant = 400
         }
+
+        self.checkFollowing()
     }
     
     @IBAction func selectFollow(sender: UIButton) {
         print("フォローボタン押した")
-        
-        let relationObject = NCMBObject(className: "Relationship")
-        relationObject.setObject(NCMBUser.currentUser(), forKey: "followed")
-        relationObject.setObject(postObject.objectForKey("user"), forKey: "follower")
-        relationObject.save(nil)
-        
-        followButton.setTitle("フォローした", forState: UIControlState.Normal)
-        followButton.titleLabel?.font = UIFont.systemFontOfSize(10)
+
+        self.checkFollowing()
+        if isFollowing == false {
+            print("フォローする")
+            let relationObject = NCMBObject(className: "Relationship")
+            relationObject.setObject(NCMBUser.currentUser(), forKey: "followed")
+            relationObject.setObject(postObject.objectForKey("user"), forKey: "follower")
+            followingRelationshipObject.objectId = relationObject.objectId
+            relationObject.save(nil)
+            self.isFollowing = true
+            print("フォローした")
+            followButton.setTitle("フォロー中", forState: UIControlState.Normal)
+            followButton.titleLabel?.font = UIFont.systemFontOfSize(12)
+        } else {
+            print("フォローをやめる")
+            followingRelationshipObject.fetchInBackgroundWithBlock({(NSError error) in
+                if (error != nil) {
+                    print(error)
+                } else {
+                    self.followingRelationshipObject.deleteInBackgroundWithBlock({(NSError error) in
+                        if (error != nil) {
+                            print(error)
+                        } else {
+                            print("フォローをやめました")
+                            self.followButton.setTitle("フォロー", forState: UIControlState.Normal)
+                            self.followButton.titleLabel?.font = UIFont.systemFontOfSize(14)
+                            self.isFollowing = false
+                            self.followingRelationshipObject.objectId = "dummy"
+                        }
+                    })
+                }
+            })
+        }
+    }
     
+//    フォローしてるかどうかの判定メソッド
+    func checkFollowing() {
+//        フォロー/フォロワーが合致してたらフォローしてるってことでRelationshipクラスのインスタンスを検索
+        let relationshipQuery: NCMBQuery = NCMBQuery(className: "Relationship")
+        relationshipQuery.whereKey("followed", equalTo: NCMBUser.currentUser())
+        relationshipQuery.whereKey("follower", equalTo: postObject.objectForKey("user"))
+
+        relationshipQuery.findObjectsInBackgroundWithBlock({(NSArray objects, NSError error) in
+            if (error != nil) {
+                print(error.localizedDescription)
+            } else {
+                if objects != nil {
+                    if objects as NSObject != [] {
+//                        フォロー/フォロワー関係の時
+                        self.followingRelationshipObject = objects[0] as! NCMBObject
+                        print("フォローしてる")
+                        self.isFollowing = true
+                        self.followButton.setTitle("フォロー中", forState: UIControlState.Normal)
+                        self.followButton.titleLabel?.font = UIFont.systemFontOfSize(12)
+                    } else {
+                        print("フォローしてない")
+                        self.isFollowing = false
+                        self.followButton.setTitle("フォロー", forState: UIControlState.Normal)
+                        self.followButton.titleLabel?.font = UIFont.systemFontOfSize(14)
+                    }
+                } else {
+                    print("フォローしてない")
+                    self.isFollowing = false
+                    self.followButton.setTitle("フォロー", forState: UIControlState.Normal)
+                    self.followButton.titleLabel?.font = UIFont.systemFontOfSize(14)
+                }
+            }
+        })
     }
     
 }
