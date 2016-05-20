bd5f5a1b087b1c21c19d2e6c0da9d63a23748f4a
FumiYama <f.yamanaka1127@gmail.com>
2016-05-20 12:57:12 +0900

    add followButton

--- OtherAccountViewController.swift
+++ OtherAccountViewController.swift
@@ -17,11 +17,42 @@ class OtherAccountViewController: UIViewController {
     @IBOutlet weak var userHomeImageView: UIImageView!
     @IBOutlet weak var userSelfIntroductionLabel: UILabel!
     
-    var user: NCMBUser!
+    @IBOutlet var otherAccountFollowButton: UIButton!
     
+    var user: NCMBUser!
+    var isFollowing: Bool = false
+    var followingRelationshipObject: NCMBObject = NCMBObject()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //        フォロー/フォロワーが合致してたらフォローしてるってことでRelationshipクラスのインスタンスを検索
+        let relationshipQuery: NCMBQuery = NCMBQuery(className: "Relationship")
+        relationshipQuery.whereKey("followed", equalTo: NCMBUser.currentUser())
+        relationshipQuery.whereKey("follower", equalTo: user)
+        relationshipQuery.findObjectsInBackgroundWithBlock({(NSArray objects, NSError error) in
+            if (error != nil) {
+                print(error.localizedDescription)
+            } else {
+                if objects != nil {
+                    if objects as NSObject != [] {
+                        //                        フォロー/フォロワー関係の時
+                        print("フォローしてる")
+                        self.followingRelationshipObject = objects[0] as! NCMBObject
+                        self.isFollowing = true
+                        self.otherAccountFollowButton.setTitle("フォロー中", forState: UIControlState.Normal)
+                    } else {
+                        print("フォローしてない")
+                        self.isFollowing = false
+                        self.otherAccountFollowButton.setTitle("フォロー", forState: UIControlState.Normal)
+                    }
+                } else {
+                    print("フォローしてない")
+                    self.isFollowing = false
+                    self.otherAccountFollowButton.setTitle("フォロー", forState: UIControlState.Normal)
+                }
+            }
+        })
         
         print(user)
         userIdLabel.text = "@" + user.userName
@@ -55,6 +86,44 @@ class OtherAccountViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func selectOtherAccountFollowButton(sender: AnyObject) {
+        print("followButton押した。")
+        
+        
+        if isFollowing == false {
+            print("フォローする")
+            let relationObject = NCMBObject(className: "Relationship")
+            relationObject.setObject(NCMBUser.currentUser(), forKey: "followed")
+            relationObject.setObject(user, forKey: "follower")
+            followingRelationshipObject.objectId = relationObject.objectId
+            relationObject.save(nil)
+            self.isFollowing = true
+            print("フォローした")
+            otherAccountFollowButton.setTitle("フォロー中", forState: UIControlState.Normal)
+        } else {
+            print("フォローをやめる")
+            followingRelationshipObject.fetchInBackgroundWithBlock({(NSError error) in
+                if (error != nil) {
+                    print(error)
+                } else {
+                    self.followingRelationshipObject.deleteInBackgroundWithBlock({(NSError error) in
+                        if (error != nil) {
+                            print(error)
+                        } else {
+                            print("フォローをやめました")
+                            self.otherAccountFollowButton.setTitle("フォロー", forState: UIControlState.Normal)
+                            self.isFollowing = false
+                            self.followingRelationshipObject.objectId = "dummy"
+                        }
+                    })
+                }
+            })
+        }
+        
+        
     }
 
 }
