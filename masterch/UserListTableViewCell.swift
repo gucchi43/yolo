fe2a42001d0c9b0b25f5a096762ad056e87008fa
FumiYama <f.yamanaka1127@gmail.com>
2016-06-07 04:53:28 +0900

    add file

---
+++ masterch/UserListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserListTableViewCell.swift
+//  masterch
+//
+//  Created by Fumiya Yamanaka on 2016/06/07.
+//  Copyright © 2016年 Fumiya Yamanaka. All rights reserved.
+//
+
+import UIKit
+
+class UserListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
