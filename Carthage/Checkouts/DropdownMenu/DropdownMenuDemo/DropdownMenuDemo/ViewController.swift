bfe2bdfcedacfe06f942ecdcc70ffad9f2641bf9
Hiroki Taniguchi <acmikn43@gmail.com>
2016-06-19 13:20:00 +0900

    DropdownMenuライブラリ導入 #139

---
+++ Carthage/Checkouts/DropdownMenu/DropdownMenuDemo/DropdownMenuDemo/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  DropdownMenuDemo
+//
+//  Created by Suric on 16/5/26.
+//  Copyright © 2016年 teambition. All rights reserved.
+//
+
+import UIKit
+import DropdownMenu
+
+class ViewController: UIViewController {
+    var selectedRow: Int = 0
+    var items: [DropdownItem]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showMenu(sender: UIBarButtonItem) {
+        let item1 = DropdownItem(title: "NO Image")
+        let item2 = DropdownItem(image: UIImage(named: "file")!, title: "File")
+        let item3 = DropdownItem(image: UIImage(named: "post")!, title: "Post", style: .Highlight)
+        let item4 = DropdownItem(image: UIImage(named: "post")!, title: "Event", style: .Highlight, accessoryImage: UIImage(named: "accessory")!)
+
+        items = [item1, item2, item3, item4]
+        let menuView = DropdownMenu(navigationController: navigationController!, items: items, selectedRow: selectedRow)
+        menuView.delegate = self
+        menuView.showMenu(onNavigaitionView: true)
+    }
+}
+
+extension ViewController: DropdownMenuDelegate {
+    func dropdownMenu(dropdownMenu: DropdownMenu, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("DropdownMenu didselect \(indexPath.row) text:\(items[indexPath.row].title)")
+        if indexPath.row != items.count - 1 {
+            self.selectedRow = indexPath.row
+        }
+
+        let alertConroller = UIAlertController(title: "Nice", message: "You choose \(items[indexPath.row].title)", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+        alertConroller.addAction(okAction)
+        presentViewController(alertConroller, animated: true) {
+            print("Display success")
+        }
+    }
+}
