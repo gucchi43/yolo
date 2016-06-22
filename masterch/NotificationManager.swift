5ef8f77bbcdea6ce3667cde29716e356148fb2be
Hiroki Taniguchi <acmikn43@gmail.com>
2016-06-22 22:23:28 +0900

    Notificatonのデータを削除するメソッドを追加 #147  - NotificationのsaveInBackgroundWithBlockをsaveEventuallyに変更（削除メソッドも同じく）

--- masterch/NotificationManager.swift
+++ masterch/NotificationManager.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class NotificationManager: NSObject {
     
+
+
+    //---------------いいね---------------
+    
     //いいねしたことを通知画面のデータに保存
     func likeNotification(user: NCMBUser, post: NCMBObject, postHeader: String){
         let notificationObject = NCMBObject(className: "Notification")
@@ -19,14 +23,35 @@ class NotificationManager: NSObject {
         let postRelation: NCMBRelation = NCMBRelation(className: notificationObject, key: "post")
         postRelation.addObject(post)
         notificationObject.setObject(postHeader, forKey: "postHeader")
-        notificationObject.fetchInBackgroundWithBlock({ (error) -> Void in
+        notificationObject.saveEventually({ (error) -> Void in
             if let error = error {
                 print("error", error.localizedDescription)
             }else {
-                print("follow: Notificationへの保存成功")
+                print("like: Notificationへの保存成功")
             }
         })
     }
+    
+    //いいねしたNotificationのデータを削除する
+    func deletelikeNotification(user: NCMBUser, post: NCMBObject, postHeader: String){
+        let notificationObject = NCMBObject(className: "Notification")
+        notificationObject.setObject(user, forKey: "ownerUser")
+        notificationObject.setObject("like", forKey: "type")
+        notificationObject.setObject(NCMBUser.currentUser(), forKey: "actionUser")
+        let postRelation: NCMBRelation = NCMBRelation(className: notificationObject, key: "post")
+        postRelation.addObject(post)
+        notificationObject.setObject(postHeader, forKey: "postHeader")
+        notificationObject.deleteEventually ({ (error) -> Void in
+            if let error = error {
+                print("error", error.localizedDescription)
+            }else {
+                print("like: Notificationへの削除成功")
+            }
+        })
+    }
+    
+    
+    //---------------フォロー---------------
     
     //フォローしたことを通知画面のデータに保存
     func followNotification(user: NCMBUser){
@@ -35,7 +60,7 @@ class NotificationManager: NSObject {
         notificationObject.setObject("follow", forKey: "type")
         notificationObject.setObject(NCMBUser.currentUser(), forKey: "actionUser")
         notificationObject.setObject(nil, forKey: "post")
-        notificationObject.fetchInBackgroundWithBlock({ (error) -> Void in
+        notificationObject.saveEventually({ (error) -> Void in
             if let error = error {
                 print("error", error.localizedDescription)
             }else {
@@ -44,6 +69,24 @@ class NotificationManager: NSObject {
         })
     }
     
+    //フォローしたNotificationのデータを削除する
+    func deleteFollowNotification(user: NCMBUser){
+        let notificationObject = NCMBObject(className: "Notification")
+        notificationObject.setObject(user, forKey: "ownerUser")
+        notificationObject.setObject("follow", forKey: "type")
+        notificationObject.setObject(NCMBUser.currentUser(), forKey: "actionUser")
+        notificationObject.setObject(nil, forKey: "post")
+        notificationObject.saveEventually({ (error) -> Void in
+            if let error = error {
+                print("error", error.localizedDescription)
+            }else {
+                print("follow: Notificationへの保存成功")
+            }
+        })
+    }
+    
+    
+    //---------------コメント---------------
     
     //commentしたことを通知画面のデータに保存
     func commentNotification(user: NCMBUser, post: NCMBObject, postHeader: String){
@@ -54,7 +97,7 @@ class NotificationManager: NSObject {
         let postRelation: NCMBRelation = NCMBRelation(className: notificationObject, key: "post")
         postRelation.addObject(post)
         notificationObject.setObject(postHeader, forKey: "postHeader")
-        notificationObject.fetchInBackgroundWithBlock({ (error) -> Void in
+        notificationObject.deleteEventually({ (error) -> Void in
             if let error = error {
                 print("error", error.localizedDescription)
             }else {
@@ -63,5 +106,25 @@ class NotificationManager: NSObject {
         })
 
     }
+    
+    //コメントしたNotificationのデータを削除する
+    func deleteCommentNotification(user: NCMBUser, post: NCMBObject, postHeader: String){
+        let notificationObject = NCMBObject(className: "Notification")
+        notificationObject.setObject(user, forKey: "ownerUser")
+        notificationObject.setObject("comment", forKey: "type")
+        notificationObject.setObject(NCMBUser.currentUser(), forKey: "actionUser")
+        let postRelation: NCMBRelation = NCMBRelation(className: notificationObject, key: "post")
+        postRelation.addObject(post)
+        notificationObject.setObject(postHeader, forKey: "postHeader")
+        notificationObject.deleteEventually({ (error) -> Void in
+            if let error = error {
+                print("error", error.localizedDescription)
+            }else {
+                print("comment: Notificationへの保存成功")
+            }
+        })
+        
+    }
+
 
 }
