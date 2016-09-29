7476900af15b6617cc7e67e029cee8973f62a818
Hiroki Taniguchi <acmikn43@gmail.com>
2016-09-29 17:34:04 +0900

    投稿する時、選択した日にちに投稿できるように #269

--- masterch/MainTabViewController.swift
+++ masterch/MainTabViewController.swift
@@ -18,42 +18,10 @@ class MainTabViewController: UITabBarController, UITabBarControllerDelegate {
         //-------タブバーのデザイン------
         //タブバーのボタンを選択した時の色を設定
         self.tabBar.tintColor = ColorManager.sharedSingleton.mainColor()
-        //Submitのボタンだけデフォルトで色付きでのせるためオリジナルの画像を表示
-        let submitTabButton = self.tabBar.items?[2]
-        submitTabButton?.image = UIImage(named: "tabSubmit")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
-    }
 
-    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
-        if viewController is SubmitDummyViewController {
-            if let selectedViewController = self.selectedViewController {
-                let storyboard = UIStoryboard(name: "Submit", bundle: nil)
-                let submitViewController = storyboard.instantiateViewControllerWithIdentifier("Submit") as! SubmitViewController
-                
-                //選択しているタブが0番目（LogViewController）の場合、選択している日付を受け渡す
-                if self.selectedIndex as Int == 0 {
-                    let navigation = selectedViewController as! UINavigationController
-                    submitViewController.delegate = navigation.topViewController as! LogViewController
-                    submitViewController.postDate = CalendarManager.currentDate
-                    submitViewController.postPickerDate = CalendarManager.currentDate
-                }else {
-                    print("何番目",self.selectedIndex as Int)
-                }
-                selectedViewController.presentViewController(submitViewController, animated: true, completion: nil)
-            }
-            
-            return false
-        }
-        return true
     }
 
     override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
-//        if selectedIndex as Int == 0{
-//            print("selectedIndex as Int",selectedIndex as Int)
-//            logManager.sharedSingleton.logNumber = 0
-//            logManager.sharedSingleton.logUser = NCMBUser.currentUser()
-//        }else {
-//            print("selectedIndex as Int",selectedIndex as Int)
-//        }
         //押したタブ(item)にbadgeがあった場合、アプリ上、データベース上共にリセットする
         if item.badgeValue != nil{
             item.badgeValue = nil
@@ -69,7 +37,7 @@ class MainTabViewController: UITabBarController, UITabBarControllerDelegate {
             print("tag ID = 2")
 //            logManager.sharedSingleton.logNumber = 2
             logManager.sharedSingleton.logTitleToggle = false
-        case 3://投稿
+        case 3://タイムトリップ
             print("tag ID = 3")
         case 4://通知
             print("tag ID = 4")
